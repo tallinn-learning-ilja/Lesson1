6cea7c735cc334352a856188191940a82718a058
nemens1s <ilja.nikolski@hotmail.com>
2023-09-21 19:46:25 +0300

    Deleted class Main

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world1!");
-        System.out.println("Hello world2!");
-        System.out.println("Hello world3!");
-    }
-}
